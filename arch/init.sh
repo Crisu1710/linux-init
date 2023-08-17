5688c57aa4ca13354002bf02ca449b70a314d535
Crisu1710 <m2rs@posteo.de>
2023-08-17 12:43:45 +0200

    use functions

--- arch/init.sh
+++ arch/init.sh
@@ -1,54 +1,95 @@
-######## install yay ########
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-##############################
-mkdir Documents GIT
+#!/bin/bash
 
-yay -S --noconfirm \
-  # settings
-  bluez \
-  bluez-utils \
-  blueberry \
-  network-manager-applet \
-  pulseaudio \
-  fwupd \
-  # general
-  zsh \
-  neofetch \
-  terminator \
-  btop \
-  bat \
-  ttf-hack-nerd-font \
-  telegram-desktop \
-  spotify \
-  meld \
-  piper \
-  discord \
-  joplin-appimage \
-  # office
-  evolution \
-  libreoffice-fresh-de \
-  chromium \
-  keepassxc \
-  flameshot \
-  # OPS/Network
-  net-tools \
-  aws-cli \
-  whois \
-  tcpdump \
-  remmina \
-  remmina-plugin-rdesktop \
-  podman \
-  # K8s
-  kubectl \
-  kube-ps1 \
-  talosctl \
-  openlens-bin \
-  k9s \
-  # Dev
-  visual-studio-code-bin \
-  neovim \
-  postman-bin \
-  dbeaver
+install-yay () {
+  pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+}
+
+make-dirs () {
+  mkdir /home/$USER/Documents
+  mkdir /home/$USER/GIT
+}
+
+install-settings () {
+  yay -S --noconfirm \
+    bluez \
+    bluez-utils \
+    blueberry \
+    network-manager-applet \
+    pulseaudio \
+    fwupd
+}
+
+install-general () {
+  yay -S --noconfirm \
+   zsh \
+   neofetch \
+   terminator \
+   btop \
+   bat \
+   ttf-hack-nerd-font \
+   telegram-desktop \
+   spotify \
+   meld \
+   piper \
+   discord \
+   joplin-appimage
+}
+
+install-office () {
+  yay -S --noconfirm \
+    evolution \
+    libreoffice-fresh-de \
+    chromium \
+    keepassxc \
+    flameshot
+}
+
+install-ops () {
+  yay -S --noconfirm \
+    net-tools \
+    aws-cli \
+    whois \
+    tcpdump \
+    remmina \
+    remmina-plugin-rdesktop \
+    podman
+}
+
+install-k8s () {
+  yay -S --noconfirm \
+    kubectl \
+    kube-ps1 \
+    talosctl \
+    openlens-bin \
+    k9s
+}
+
+install-dev () {
+  yay -S --noconfirm \
+    visual-studio-code-bin \
+    neovim \
+    postman-bin \
+    dbeaver
+}
+
+echo "make std dirs"
+make-dirs
+echo "install yay"
+install-yay
+echo "install office"
+install-office
+echo "install extension"
+install-extension
+echo "install settings"
+install-settings
+echo "install dev"
+install-dev
+echo "install k8s"
+install-k8s
+echo "install ops"
+install-ops
+echo "install general"
+install-general
 
 # OH-MY-ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
