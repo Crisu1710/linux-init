7cec13f6669a22da3a432aa4f1f50bcc1790e902
Crisu1710 <m2rs@posteo.de>
2023-08-17 16:42:51 +0000

    .

--- arch/init.sh
+++ arch/init.sh
@@ -22,7 +22,6 @@ install-settings () {
 install-general () {
   yay -S --noconfirm \
    zsh \
-   fzf \
    lsd \
    neofetch \
    terminator \
@@ -93,15 +92,16 @@ install-ops
 echo "install general"
 install-general
 
-# OH-MY-ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#### OH-MY-ZSH
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-# NvChad 
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+#git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+#### NvChad 
+#git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
 # bluetooth
 systemctl enable bluetooth
