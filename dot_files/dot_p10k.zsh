b91dc571c99820c2875abb65739c02436d6006e3
Crisu1710 <m2rs@posteo.de>
2023-09-04 14:55:58 +0200

    adjust GO icon

--- dot_files/dot_p10k.zsh
+++ dot_files/dot_p10k.zsh
@@ -1117,12 +1117,12 @@
 
   #######################[ go_version: go version (https://golang.org) ]########################
   # Go version color.
-  typeset -g POWERLEVEL9K_GO_VERSION_FOREGROUND=255
-  typeset -g POWERLEVEL9K_GO_VERSION_BACKGROUND=2
+  typeset -g POWERLEVEL9K_GO_VERSION_FOREGROUND=14
+  typeset -g POWERLEVEL9K_GO_VERSION_BACKGROUND=08
   # Show go version only when in a go project subdirectory.
   typeset -g POWERLEVEL9K_GO_VERSION_PROJECT_ONLY=true
   # Custom icon.
-  # typeset -g POWERLEVEL9K_GO_VERSION_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_GO_VERSION_VISUAL_IDENTIFIER_EXPANSION=''
 
   #################[ rust_version: rustc version (https://www.rust-lang.org) ]##################
   # Rust version color.
