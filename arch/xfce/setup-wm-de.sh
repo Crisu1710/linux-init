34280be931fc71c32099d8ecb88de08febb21374
Robin Schneider <m2rs@posteo.de>
2023-11-27 21:55:11 +0100

    fix xfconf-query xfce4-keyboard-shortcuts

--- arch/xfce/setup-wm-de.sh
+++ arch/xfce/setup-wm-de.sh
@@ -3,7 +3,7 @@ yay -S --noconfirm\
   papirus-icon-theme
 
 
-xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/Super_L -s "xfce4-popup-whiskermenu"
+xfconf-query -c xfce4-keyboard-shortcuts -p /commands/custom/Super_L -s "xfce4-popup-whiskermenu" -n -t string
 xfconf-query -c xsettings -p /Net/ThemeName -s  "Matcha-dark-azul"
 xfconf-query -c xsettings -p /Net/IconThemeName -s  "Papirus-Dark"
 xfconf-query -c xfwm4 -p /general/theme -s "Matcha-dark-azul"
